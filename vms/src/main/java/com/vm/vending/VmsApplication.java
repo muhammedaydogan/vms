0ba69c8979676026b39cb0c749051f9f968ff25c
muhammedyaydogan <muhammed.aydogan@aol.com>
2025-03-22 19:36:42 +0300

    reorganized file structure

--- vms/src/main/java/com/vm/vending/VmsApplication.java
+++ vms/src/main/java/com/vm/vending/VmsApplication.java
@@ -1,4 +1,4 @@
-package com.example.vms;
+package com.vm.vending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- vms/src/test/java/com/vm/vending/VmsApplicationTests.java
+++ vms/src/test/java/com/vm/vending/VmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.vms;
+package com.vm.vending;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
