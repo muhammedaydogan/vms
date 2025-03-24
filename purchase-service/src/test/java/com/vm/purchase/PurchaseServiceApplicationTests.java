b944dd0bbbaba1fc710bbdfaae3cf1e508fa8bc6
muhammedyaydogan <muhammed.aydogan@aol.com>
2025-03-24 03:36:33 +0300

    added empty purchase-service & updated pom.xml

---
+++ purchase-service/src/test/java/com/vm/purchase/PurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
